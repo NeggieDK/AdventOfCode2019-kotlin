341b83dd4ea8754d43b497a8129458eb85cab589
neggie <aaron.dekoninck@gmail.com>
2019-12-25 00:04:43 +0100

    Day 16: added some extra info

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,15 +1,13 @@
 package day16
 
-import java.util.*
 import kotlin.math.abs
 
 var inputSignal = "59773775883217736423759431065821647306353420453506194937477909478357279250527959717515453593953697526882172199401149893789300695782513381578519607082690498042922082853622468730359031443128253024761190886248093463388723595869794965934425375464188783095560858698959059899665146868388800302242666479679987279787144346712262803568907779621609528347260035619258134850854741360515089631116920328622677237196915348865412336221562817250057035898092525020837239100456855355496177944747496249192354750965666437121797601987523473707071258599440525572142300549600825381432815592726865051526418740875442413571535945830954724825314675166862626566783107780527347044".toList().map { it.toString().toInt() }.toMutableList()
 var phases = 100
 
-var output = mutableListOf<Int>()
 fun main() {
     //Part 1
-    println(getOutput(100, inputSignal).subList(0,7))
+    println("Part 1: ${getOutput(100, inputSignal).subList(0,8)}")
 
     //Part 2
     val startingSize = inputSignal.size
@@ -19,6 +17,13 @@ fun main() {
     val offset = parseOffset()
     if(offset < inputSignal.size/2) throw IllegalArgumentException()
     //second half
+    //For part 2 you need to find 8 digits but only after a certain offset
+    //This offset is bigger than half of the input (even when multiplied 10_000 times)
+    //You can calculate the second half of the numbers much faster
+    //Because of the repeating pattern, the values that have to be multiplied will be divided in 0,0..0,1,1...1, so the first half of the input values will be multiplied by 0 so they don't have to be calculated
+    //The repeating pattern of the last output will look like 0,0..0,1 (only the last element is a 1) meaning that the last digit of the output is always the same!
+    //If you then want to calculate the second to last item, you can add the previous item + the current item and you have the second the last item
+    //because the repeating pattern looks like 0,0..0,1,1 (all zeroes, except last 2) meaning that only the last 2 digits matter!
     var inputSignalSublist = inputSignal.subList(offset, inputSignal.size)
     for (k in 0 until phases) {
         val output = mutableListOf<Int>()
@@ -31,11 +36,11 @@ fun main() {
         inputSignalSublist.reverse()
         output.clear()
     }
-    println(inputSignalSublist.subList(0,8))
+    println("Part 2: ${inputSignalSublist.subList(0,8)}")
 }
 
 fun parseOffset(): Int{
-    var offsetList = inputSignal.subList(0, 7)
+    val offsetList = inputSignal.subList(0, 7)
     var tempString = ""
     offsetList.forEach {
         tempString += it.toString()
@@ -59,10 +64,6 @@ fun getOutput(phases: Int, input: MutableList<Int>): List<Int>{
     return inputSignal
 }
 
-fun getSecondHalf(){
-
-}
-
 fun getRepeatValue(index: Int, repeat: Int): Int {
     val moduloValue = repeat * 4 //4 because the repeating array has 4 elements
     val index = index + 1
@@ -79,23 +80,4 @@ fun getRepeatValue(index: Int, repeat: Int): Int {
         3 -> -1
         else -> throw IllegalArgumentException()
     }
-}
-
-fun getRepeatingPattern(repeatingPattern: List<Int>, repeat: Int, size: Int): MutableList<Int> {
-    val repeatingPatternList = mutableListOf<Int>()
-    var amountRepeated = 0
-    var iterator = 0
-    while (repeatingPatternList.size != size + 1) { //size + 1 because we have to remove the first element of the list at the end
-        repeatingPatternList.add(repeatingPattern[iterator])
-        amountRepeated++
-        if (amountRepeated >= repeat) {
-            iterator++
-            amountRepeated = 0
-            if (iterator == repeatingPattern.size) {
-                iterator = 0
-            }
-        }
-    }
-    repeatingPatternList.removeAt(0)
-    return repeatingPatternList
 }
